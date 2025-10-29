8d85daba246db8eb891e556f9b40021cd2ae64ca
Evolve <jatina@evolvedigitas.com>
2025-10-29 14:12:45 +0530

    Cors setting update

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,27 @@ import dotenv from "dotenv";
 import apiRoutes from "./routes/apiRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
-dotenv.config({
-  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env"
-});
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: process.env.FRONTEND_URL,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],              
-  allowedHeaders: ["Content-Type", "Authorization"],                 
-  credentials: true
-};
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
+// app.use(cors(corsOptions));
 
 app.use("/api", apiRoutes);
 app.use("/auth", authRoutes);
